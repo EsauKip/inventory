591d978dffad8de19fb1ae3d3251bee169e113cc
EsauKip <esau.kiprono@student.moringaschool.com>
2022-10-04 10:58:59 +0300

    create the inventory model

---
+++ app_control/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from userint.models import CustomUser
+from userint.views import add_user_activity
+# Create your models here.
+
+class InventoryGroup(models.Model):
+    created_by=models.ForeignKey(CustomUser, null=True, related_name="inventory_groups",
+    on_delete=models.SET_NULL)
+    name = models.CharField(max_length=100,unique=True)
+    belongs_to =models.ForeignKey('self',null=True,on_delete=models.SET_NULL,related_name="group_relations")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering =("-created_at")
+    def __init__(self, *args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.old_name = self.name    
+    def save(self, *args, **kwargs):
+        action = f"add new group - '{self.name}'"
+        if self.pk is not None:
+            action = f"updated group from - '{self.old_name}' to '{self.name}'" 
+        super().save(*args,**kwargs)   
+        add_user_activity(self.created_by,action=action)
+
+    def delete(self,*args,**kwargs):
+        created_by=self.created_by
+        action = f"deleted group - '{self.name}'"
+        super().delete(*args,**kwargs)
+        add_user_activity(created_by,action=action)        
+
+    def __str__(self):
+        return self.name
+        
+
--- inventory/settings.py
+++ inventory/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'userint'
+    'userint',
+    'app_control',
 ]
 
 MIDDLEWARE = [
