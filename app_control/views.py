b9c8c1b322579189deed4b4a69675927e3d88a00
EsauKip <esau.kiprono@student.moringaschool.com>
2022-10-26 12:55:48 +0300

    add invoice views

--- app_control/views.py
+++ app_control/views.py
@@ -1,7 +1,7 @@
 from ast import keyword
 from unittest import result
 from rest_framework.viewsets import ModelViewSet
-from .serializers import (Inventory,InventoryGroup,InventoryGroupSerializer,InventorySerializer,Shop,ShopSerializer)
+from .serializers import (Inventory,InventoryGroup,InventoryGroupSerializer,InventorySerializer,Shop,ShopSerializer,Invoice,InvoiceSerializer)
 from rest_framework.response import Response
 from inventory.custom_methods import IsAuthenticatedCustom
 from inventory.utils import CustomPagination,get_query
@@ -130,4 +130,31 @@ class InventoryGroupView(ModelViewSet):
         request.data.update({"created_by_id":request.user.id})
         return super().create(request, *args,**kwargs)    
 
+class InvoiceView(ModelViewSet):
+    queryset = Invoice.select_related("created_by","shop").prefetch_related("invoice_items")
+    serializer_class = InvoiceSerializer
+    permission_classes=(IsAuthenticatedCustom,)
+    pagination_class=CustomPagination
+
+    def get_queryset(self):
+        if self.request.method.lower() !="get":
+            return self.queryset
+
+        data = self.request.query_params.dict()
+        data.pop("page")
+        keyword=data.pop("keyword",None)
+
+        results=self.queryset(**data)
+
+        if keyword:
+            search_fields = (
+                "created_by__fullname","shop__name","created_by__email"
+            )   
+            query=get_query(keyword,search_fields)
+            results=results.filter(query)
+        return results
+    def create(self,request, *args,**kwargs):
+
+        request.data.update({"created_by_id":request.user.id})
+        return super().create(request, *args,**kwargs)
 
